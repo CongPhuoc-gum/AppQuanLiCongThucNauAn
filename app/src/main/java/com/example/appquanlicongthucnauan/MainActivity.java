75255e281c5a3d1db550f59da3bea282df840c3b
Hoang Quoc Viet <viethoangquoc2004@gmail.com>
2024-10-15 16:04:59 +0700

    update

--- app/src/main/java/com/example/appquanlicongthucnauan/MainActivity.java
+++ app/src/main/java/com/example/appquanlicongthucnauan/MainActivity.java
@@ -1,24 +1,51 @@
 package com.example.appquanlicongthucnauan;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.appquanlicongthucnauan.login_register.login_fragment;
+import com.example.appquanlicongthucnauan.login_register.register;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn_login;
+    Button btn_register;
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+
+        btn_login = findViewById(R.id.button_login);
+        btn_register = findViewById(R.id.button_register);
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+
+             @Override
+             public void onClick(View view) {
+                 btn_login.setVisibility(View.GONE);
+             Fragment fragment = new login_fragment();
+             FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+              fragmentTransaction.replace(R.id.main,fragment).commit();
+             }
+        }
+
+        );
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                btn_register.setVisibility(View.GONE);
+                Fragment fragment = new register();
+                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.main,fragment).commit();
+            }
         });
     }
+
 }
