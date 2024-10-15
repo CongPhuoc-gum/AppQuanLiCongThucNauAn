346388e3eca743a8f6d14de09761aadd66ce107a
Hoang Quoc Viet <viethoangquoc2004@gmail.com>
2024-10-15 22:21:41 +0700

    update

--- app/src/main/java/com/example/appquanlicongthucnauan/addct.java
+++ app/src/main/java/com/example/appquanlicongthucnauan/addct.java
@@ -4,16 +4,40 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class addct extends Fragment {
     @Nullable
+    Button button_submit;
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_addct,container, false);
+        button_submit = view.findViewById(R.id.button_submit);
+        button_submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(), "Them thanh cong ", Toast.LENGTH_SHORT).show();
+                FragmentManager fragmentManager = getParentFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                Fragment submit = new home();
+                FragmentTransaction fm = getActivity().getSupportFragmentManager().beginTransaction();
+                fm.addToBackStack(null);
+                fm.replace(R.id.fm_addct, submit).commit();
+            }
+        });
+
+
+
+
+
+
         return  view;
     }
 }
