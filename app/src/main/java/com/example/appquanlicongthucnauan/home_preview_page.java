e719b0b64373565b978efb5e514deab6a4ce35ff
CongPhuoc-gum <phancongphuoc241204@gmail.com>
2024-10-30 22:46:09 +0700

    s

--- app/src/main/java/com/example/appquanlicongthucnauan/home_preview_page.java
+++ app/src/main/java/com/example/appquanlicongthucnauan/home_preview_page.java
@@ -13,13 +13,13 @@ import androidx.fragment.app.FragmentManager;
 public class home_preview_page extends Fragment {
 
     ImageButton btn_back;
-
+    CardView cv_view;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home_preview_page, container, false);
         btn_back = view.findViewById(R.id.btn_back);
-
+        cv_view = view.findViewById(R.id.cv_profile);
         // Xử lý sự kiện cho btn_back để quay lại fragment trước đó
         btn_back.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,6 +28,13 @@ public class home_preview_page extends Fragment {
                 if (fragmentManager != null) {
                     fragmentManager.popBackStack();
                 }
+            }
+        });
+
+        cv_view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentManager fragmentManager = getParentFragmentManager()
             }
         });
 
